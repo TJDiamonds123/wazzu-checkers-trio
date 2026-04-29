17b9c9815c88a78efd05bf7c69e5b0d1ed73e82d
kaysendk <kasen.koide@wsu.edu>
2026-04-28 19:20:25 -0700

    Add files via upload

---
+++ board.cpp
@@ -0,0 +1,35 @@
+#include "Board.hpp"
+
+
+
+Board::Board(int s) {
+	size = s;
+}
+
+
+void Board::create(sf::RenderWindow& window) {
+	for (int r = 0; r < 8; r++) {//this is for the rows
+		for (int c = 0; c < 8; c++) {
+			sf::RectangleShape square(sf::Vector2f(size, size));//create squares
+
+			square.setPosition(c * size, r * size);
+
+
+			if ((r + c) % 2 == 0) {
+				square.setFilColor(sf::Color::White);
+
+			}
+			else {
+				square.setFilColor(sf::Color::Black);
+			}
+
+			window.draw(square);
+		}
+
+
+
+
+
+	}
+}
+
