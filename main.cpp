9c874fd74c9db8818d47dc5425ec22ce7cfea99b
Smauts <101488168+Smauts@users.noreply.github.com>
2026-04-28 22:23:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "Board.hpp"
+#include <SFML/Graphics.hpp>
 
-
+using namespace std;
+using namespace sf;
 
 int main() {
 
 
-	sf::RenderWindow window(sf::VideoMode(800, 800), " Checker Board");
+	sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 800)), "Checkers Board");
 
 	int size = 100;
 
@@ -15,17 +17,19 @@ int main() {
 
 	while (window.isOpen()) {
 
-		while( autor event = window.pollEvent()){
+		while (auto event = window.pollEvent()) {
+
+			window.clear();
+			board.create(window);
+			window.display();
+
+
 			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
 
 		}
 
-
-		window.clear();
-		board.draw(window);
-		window.display();
 
 
 
