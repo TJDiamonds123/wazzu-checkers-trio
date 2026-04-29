786a88ea520f35aef2202791a64d328c3ea73a1e
Smauts <101488168+Smauts@users.noreply.github.com>
2026-04-29 01:08:03 -0700

    Create NormalPiece.hpp

---
+++ NormalPiece.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Piece.hpp"
+
+class NormalPiece : public Piece
+{
+private:
+    bool red;
+
+public:
+    NormalPiece(bool isRed) : red(isRed) {}
+
+    void draw(sf::RenderWindow& window, int x, int y, int size) override
+    {
+        sf::CircleShape piece(size / 2.5f);
+
+        piece.setPosition(sf::Vector2f(
+            (float)(x + 15),
+            (float)(y + 15)
+        ));
+
+        piece.setFillColor(red ? sf::Color::Red : sf::Color::Black);
+
+        window.draw(piece);
+    }
+};
