46ac30737d5c690e66944d8f93da5e4dd6f424fb
kaysendk <kasen.koide@wsu.edu>
2026-04-28 19:20:47 -0700

    Add files via upload

---
+++ board.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+
+
+
+#include <SFML/Graphics.hpp>
+
+class Board {
+private: int size; 
+
+
+
+public:
+ Board(int s);
+ void create(sf::RenderWindow& window);
+
+
+
+
+
+};
+
+
+
+
+
+
