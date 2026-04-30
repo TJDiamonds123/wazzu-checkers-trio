8481fed67cc39501f4fae7c9fefacf14aefd8818
Smauts <101488168+Smauts@users.noreply.github.com>
2026-04-29 18:32:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include "Board.hpp"
 #include <SFML/Graphics.hpp>
+#include <string>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 800)), "Checkers");
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "Checkers");
 
     int size = 100;
     Board board(size);
 
-    std::string winner = "\0";
+    bool menu = true;
+    std::string winner = "";
 
     while (window.isOpen())
     {
@@ -22,34 +24,66 @@ int main()
             if (event->is<sf::Event::MouseButtonPressed>())
             {
                 auto pos = sf::Mouse::getPosition(window);
+                int x = pos.x;
+                int y = pos.y;
 
-                int col = pos.x / size;
-                int row = pos.y / size;
+                if (menu)
+                {
+                    if (x > 250 && x < 550 && y > 250 && y < 350)
+                    {
+                        menu = false;
+                    }
+                    else if (x > 250 && x < 550 && y > 400 && y < 500)
+                    {
+                        window.close();
+                    }
+                }
+                else
+                {
+                    int col = x / size;
+                    int row = y / size;
 
-                board.whenClick(row, col);
+                    board.whenClick(row, col);
+                }
             }
         }
 
-        if (board.getBlack() == 0)
+        // WIN CHECK
+        if (!menu)
         {
-            winner = "Red Wins!";
-        }
-        else if (board.getRed() == 0)
-        {
-            winner = "Black Wins!";
+            if (board.getBlack() == 0)
+                winner = "Red Wins!";
+            else if (board.getRed() == 0)
+                winner = "Black Wins!";
         }
 
         window.clear();
-        board.create(window);
 
-        if (winner != "\0")
+        if (menu)
         {
-            board.drawWinner(window, winner);
+            sf::RectangleShape start(sf::Vector2f(300.f, 100.f));
+            start.setPosition(sf::Vector2f(250.f, 250.f));
+            start.setFillColor(sf::Color::Green);
+
+            sf::RectangleShape exit(sf::Vector2f(300.f, 100.f));
+            exit.setPosition(sf::Vector2f(250.f, 400.f));
+            exit.setFillColor(sf::Color::Red);
+
+            window.draw(start);
+            window.draw(exit);
+        }
+        else
+        {
+            board.create(window);
+
+            if (!winner.empty())
+            {
+                board.drawWinner(window, winner);
+            }
         }
 
         window.display();
     }
 
     return 0;
-;
 }
