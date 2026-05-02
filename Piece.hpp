ef12442098a478b6ff7e747624328a4b9b66fd94
kaysendk <kasen.koide@wsu.edu>
2026-05-01 17:54:38 -0700

    Update Piece.hpp

--- Piece.hpp
+++ Piece.hpp
@@ -4,7 +4,7 @@
 class Piece
 {
 public:
-    virtual ~Piece() {}
-
+    virtual ~Piece() {}//virtural destrctor  for cleanup
+//pure virtural function that makes all piece class to implement their own draw. polymorphism is used so the pieces are different
     virtual void draw(sf::RenderWindow& window, int x, int y, int size) = 0;
 };
