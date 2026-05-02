7c9303a8a18644b3091b9275c50d11321619b18d
Smauts <101488168+Smauts@users.noreply.github.com>
2026-05-01 18:12:35 -0700

    Add files via upload

---
+++ Checkers/KingPiece.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "Piece.hpp"
+
+class KingPiece : public Piece
+{
+private:
+    bool red;
+
+public:
+    KingPiece(bool isRed) : red(isRed) {}
+
+    void draw(sf::RenderWindow& window, int x, int y, int size) override
+    {
+        sf::CircleShape piece(size / 2.5f);
+
+        piece.setPosition(sf::Vector2f((float)(x + 15), (float)(y + 15)));
+
+        if (red)
+        {
+            piece.setFillColor(sf::Color(255, 80, 80));
+        }
+        else
+        {
+            piece.setFillColor(sf::Color(70, 70, 70));
+        }
+
+        piece.setOutlineThickness(4.f);
+        piece.setOutlineColor(sf::Color::Yellow);
+
+        window.draw(piece);
+    }
+};
